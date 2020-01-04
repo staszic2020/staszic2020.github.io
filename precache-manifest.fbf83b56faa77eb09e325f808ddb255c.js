493f9bfc07024432b507dc8f4c772474b4ff3987
Piotr Grynfelder <dev.pitek@gmail.com>
2020-01-04 21:17:19 +0100

    Updates

---
+++ precache-manifest.fbf83b56faa77eb09e325f808ddb255c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ab4203043000ca835942e57d17aae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7904f157cffe41dcc6e5",
+    "url": "/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "c66354121b81beedae8a",
+    "url": "/static/css/main.56e3116f.chunk.css"
+  },
+  {
+    "revision": "7904f157cffe41dcc6e5",
+    "url": "/static/js/2.88ab7366.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.88ab7366.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c66354121b81beedae8a",
+    "url": "/static/js/main.c5088890.chunk.js"
+  },
+  {
+    "revision": "8fcbd0075710519c7784",
+    "url": "/static/js/runtime-main.e3ba0504.js"
+  }
+]);
